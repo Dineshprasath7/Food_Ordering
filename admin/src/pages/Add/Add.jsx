c9db0e69c4a43a61dedb382ed7e3eb28f8eb4c24
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2024-08-03 19:26:29 +0530

    admin changes

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import './Add.css'
 import {assets} from '../../assets/assets'
+import axios from "axios"
 
 const Add = () => {
+
+    const url="http://localhost:4000";
     const[image,setImage] = useState(false);
     const [data,setData] = useState({
         name:"",
@@ -23,6 +26,18 @@ const Add = () => {
         formData.append("price",Number(data.price))
         formData.append("category",data.category)
         formData.append("image",image)
+        const response = await axios.post(`$(url)/api/food/Add`,formData)
+        if(response.data.success){
+            setData({
+                name:"",
+                description:"",
+                price:"",
+                category:"Salad"
+            })
+        }
+        else{
+            setImage(false)
+        }
     }
   return (
     <div className='add'>
