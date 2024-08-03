4924e87ff73fc44466baec399a42e40fa9010cf8
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2024-08-03 19:31:30 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://eager9797:HackHive123@cluster0.zwwp6hn.mongodb.net/FoodDelivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://jaishreekannan3107:Yxs8QHy51EuZ0klM@cluster0.hazoilo.mongodb.net/food-del').then(() => console.log("DB Connected"));
 }
-
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,27 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
-
+//app config
 const app = express()
 const port = 4000
 
-//mw
+// middleware
 app.use(express.json())
 app.use(cors())
 
-//db connection
+//DB Connection
 connectDB();
 
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res) =>{
     res.send("API Working")
 })
 
-app.listen(port,()=>{
+app.listen(port,() => {
     console.log(`Server Started on http://localhost:${port}`)
 })
-
-//mongodb+srv://eager9797:HackHive123@cluster0.zwwp6hn.mongodb.net/?
