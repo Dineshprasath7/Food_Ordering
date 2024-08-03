1bbda0f347d088576a9b5e68aad139bf24a74c70
MuthuMeenakshi403 <muthumeenakshi403@gmail.com>
2024-08-03 22:51:52 +0530

    minor fix

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -2,9 +2,24 @@ import React, { useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
 
+
 const LoginPopup = ({setShowLogin}) => {
 
     const[currState,setCurrState] = useState("Login")
+    const [data,setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+
+
+    
   return (
     <div className='login-popup'>
         <form className="login-popup-container">
@@ -13,10 +28,10 @@ const LoginPopup = ({setShowLogin}) => {
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
         <div className="login-popup-inputs">
-            {currState==="Login"?<></>:<input type="text" placeholder='Your Name' required />}
-            <input type="email" placeholder='Your Email' required/>
-            <input type="password" placeholder='Password' required/>
-            <input type="password" placeholder='Retype Password' required/>
+            {currState==="Login"?<></>:<input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your Name' required />}
+            <input type="email" onChange={onChangeHandler} value={data.email} placeholder='Your Email' required/>
+            <input type="password" onChange={onChangeHandler} value={data.password} placeholder='Password' required/>
+            
 
         </div>
         <button>{currState==="Sign Up"?"Create account":"Login"}</button>
