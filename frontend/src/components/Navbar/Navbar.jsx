a35df91aceae471bf37b33528592b87eb68fec5b
MuthuMeenakshi403 <muthumeenakshi403@gmail.com>
2024-08-05 19:26:30 +0530

    error fix

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -27,11 +27,20 @@ const Navbar = ({setShowLogin}) => {
             <div className={getTotalCartAmount()===0?"":"dot"}>  
             </div>
         </div>
-        
-        
+        {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
+        :<div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className='nav-profile-dropdown'>
+            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr/>
+            <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+          </ul>
+          </div>}
       </div> 
     </div>
   )
 }
 
+
 export default Navbar
+
